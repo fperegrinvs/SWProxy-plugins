481856cadc39599df8c01c5ec791610354ac4845
Leonardo Stern <leonardo.stern@gmail.com>
2016-03-29 08:39:35 -0300

    support for multiple users #13 #14

--- plugins/RaidLogger.py
+++ plugins/RaidLogger.py
@@ -38,20 +38,31 @@ class RaidLogger(SWPlugin.SWPlugin):
             return self.log_end_battle(req_json, resp_json, config)
 
         if command == 'BattleRiftOfWorldsRaidStart':
-            config['start'] = int(time.time())
-            config['stage'] = '%s R%s' % (identify_raid(resp_json['battle_info']['room_info']['raid_id']), resp_json['battle_info']['stage_id'])
-            config['wizard'] = resp_json['wizard_info']['wizard_id']
+            if 'raid-logger-data' not in config:
+                config['raid-logger-data'] = {}
+
+            plugin_data = config['raid-logger-data']
+            wizard_id = str(resp_json['wizard_info']['wizard_id'])
+            start = int(time.time())
+            stage = '%s R%s' % (identify_raid(resp_json['battle_info']['room_info']['raid_id']), resp_json['battle_info']['stage_id'])
             team = []
             for i in resp_json['battle_info']['user_list']:
-                if i['wizard_id'] != resp_json['wizard_info']['wizard_id']: team.append(i['wizard_name']) 
-            config['team'] = team
+                if i['wizard_id'] != resp_json['wizard_info']['wizard_id']:
+                    team.append(i['wizard_name'])
+
+            plugin_data[wizard_id] = {'stage' : stage, 'start': start, 'team': team}
 
     def log_end_battle(self, req_json, resp_json, config):
         if not config["log_raids"]:
             return
 
-        if 'start' in config:
-            delta = int(time.time()) - config['start']
+        wizard_id = str(resp_json['wizard_info']['wizard_id'])
+        if 'raid-logger-data' in config and wizard_id in config['raid-logger-data'] \
+                and 'start' in config['raid-logger-data'][wizard_id]:
+
+            start_data = config['raid-logger-data'][wizard_id]
+
+            delta = int(time.time()) - start_data['start']
             m = divmod(delta, 60)
             s = m[1]  # seconds
             elapsed_time = '%s:%02d' % (m[0], s)
@@ -59,16 +70,18 @@ class RaidLogger(SWPlugin.SWPlugin):
             elapsed_time = 'N/A'
 
         win_lost = 'Win' if resp_json["win_lose"] == 1 else 'Lost'
-        log_entry = "%s,%s,%s,%s,%s,%s," % (time.strftime("%Y-%m-%d %H:%M"), config['stage'] if 'stage' in config else 'unknown', win_lost, elapsed_time, config['team'][0], config['team'][1])
+        log_entry = "%s,%s,%s,%s,%s,%s," % (time.strftime("%Y-%m-%d %H:%M"), start_data['stage'] if 'stage' in start_data else 'unknown',
+                                            win_lost, elapsed_time, start_data['team'][0], start_data['team'][1])
 
         if win_lost == 'Win':
             order = 0
             for i in resp_json['battle_reward_list']:
-                if config['wizard'] != i['wizard_id']: order += 1 
-                else: break
+                if wizard_id != str(i['wizard_id']):
+                    order += 1
+                else:
+                    break
+
             reward = resp_json['battle_reward_list'][order]['reward_list'][0]
-            print reward
-            print type(reward)
             if reward['item_master_type'] == 1:
                 log_entry += "%s" % 'Rainbowmon 3*'
             elif reward['item_master_type'] == 6:
@@ -93,7 +106,7 @@ class RaidLogger(SWPlugin.SWPlugin):
             else:
                 log_entry += "Unknown drop %s" % json.dumps(reward)
 
-        filename = config['log_raids_filename']
+        filename = "%s-raids.csv" % wizard_id
         if not os.path.exists(filename):
             log_entry = 'Date,Raid,Result,Clear time,Teammate #1,Teammate #2,Drop,Sell Value,Rune Set,Rarity,Stat\n' + log_entry
 
